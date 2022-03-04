af1783d1535f840fa573d94d209d249c00afcb10
29438 <47177190+29438@users.noreply.github.com>
2022-03-04 18:31:21 +0800

    Add files via upload

---
+++ page/time/sy.js
@@ -0,0 +1,12 @@
+window.onload = function(){
+  var winHeight = 0;
+  if (window.innerHeight){
+    winHeight = window.innerHeight;
+  }else if ((document.body) && (document.body.clientHeight)){
+    winHeight = document.body.clientHeight;
+  }
+  var html = document.getElementsByTagName('html')[0];
+  if(document.body.offsetHeight < windowHeight){
+      html.style.height = windowHeight;
+  }
+};
