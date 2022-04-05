2ba3acaf42caba9e5f93591bedbcd55881bc210d
29438 <47177190+29438@users.noreply.github.com>
2022-04-05 12:50:42 +0800

    Update tz.js

--- js/tz.js
+++ js/tz.js
@@ -1,5 +1,6 @@
 //window.location.replace("#")
-	window.onload = function(){
-		window.location.href= "https://29438.github.io/";
-	}
-alert("Hello\nHow are you?");
+//	window.onload = function(){
+//		window.location.href= "https://29438.github.io/";
+//	}
+window.open('https://29438.github.io/','_blank');
+//alert("Hello\nHow are you?");
