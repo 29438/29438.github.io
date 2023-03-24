c0d650b7dbd79bbcec31ebc1b8b3befd9f0b02e1
29438 <47177190+29438@users.noreply.github.com>
2023-03-24 22:37:37 +0800

    Add files via upload

--- configs.js
+++ configs.js
@@ -5,26 +5,26 @@ YL.static = {
   iconBtnStart: 'windows', //主图标
   author: '程鹏',//作者
   contactInformation: 'QQ: 2943871928',//联系方式
-  officialWebsite: 'www.5luck.cn',//软件官网
-  welcome: '本网站UI由 CP 强力驱动\n更多信息：www.5luck.cn',//加载完毕控制台提示信息
-  copyrightDetail: 'CopyRight©程鹏',//版权详细信息
+  officialWebsite: 'https://cpddd.tk',//软件官网
+  welcome: '本网站UI由 CP 强力驱动\n更多信息：cpddd.tk',//加载完毕控制台提示信息
+  copyrightDetail: 'CopyRight©Chengpeng',//版权详细信息
   otherStatements: '网站开发',//其他信息（可留空）
 
   /**————————————————————————————————————————————————————————————————————————————————————————————*/
-  /** YLUI基础设置 */
+  /** 基础设置 */
   lang: 'zh-cn', //语言
   localStorageName: "ylui-storage", //ls存储名
   lockedApps: [''], // 锁定的应用（不允许被脚本修改）
   trustedApps: ['yl-server'], // 受信任的应用（可以使用敏感API）
   debug: false,//启用更多调试信息
-  beforeOnloadEnable: true,//启用关闭前询问（打包app时请关闭防止出错）
+  beforeOnloadEnable: false,//启用关闭前询问（打包app时请关闭防止出错）
   WarningPerformanceInIE: true,//在IE下提示体验不佳信息
   languages: {}, //推荐留空，自动从文件加载
   changeable: true,//存档数据是否可被普通用户修改
   dataCenter: true,//是否展示数据管理中心
 
   /**————————————————————————————————————————————————————————————————————————————————————————————*/
-  /** YLUI注册信息 */
+  /** 注册信息 */
   authorization: '正版',//授权类型
   serialNumber: 2943871928,//序列号
 
